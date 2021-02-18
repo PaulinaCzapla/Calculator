0bb2db3ec3194c5f256b8699347c4a34b6c2b269
PaulinaCzapla <PaulinaCzapla@users.noreply.github.com>
2021-02-18 22:26:08 +0100

    some improvements
    - "sqrt()" button name is now "sqrt" (it's more adequate)
    - bugs with negative numbers fixed
    - bugs with "sqrt" fixed
    - bugs with "," fixed

--- calculator/MainWindow.xaml.cs
+++ calculator/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ namespace calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string memory;
+        static private string memory;
         public MainWindow()
         {
             InitializeComponent();
@@ -20,7 +20,7 @@ namespace calculator
             ResultText.Text = string.Empty;
 
             var button = sender as System.Windows.Controls.Button;
-            var currentNumber = button.Name[button.Name.Length -1];
+            char currentNumber = button.Name[button.Name.Length -1];
             CurrentOperationText.Text += currentNumber;
         }
 
@@ -28,8 +28,10 @@ namespace calculator
         {
             ResultText.Text = string.Empty;
             if(CheckPreviousOperation(sender, e))
-            CurrentOperationText.Text += "sqrt";
-            
+            {
+                string operation = CurrentOperationText.Text + "sqrt";
+                CurrentOperationText.Text = CalculateResult(operation);
+            }
         }
 
         private void ButtonPower_Click(object sender, RoutedEventArgs e)
@@ -71,25 +73,26 @@ namespace calculator
         {
             if (ContainsOperation(CurrentOperationText.Text))
             {
-                string result = CalculateResult(CurrentOperationText.Text);
-                if (result == "error")
+                string[] elements = CurrentOperationText.Text.Split('-','+','*','^','/');   
+                if (elements[elements.Length - 2] != string.Empty)
                 {
-                    ResultText.Text = result;
-                    return false;
+                    string result = CalculateResult(CurrentOperationText.Text);
+                    if (result == "error")
+                    {
+                        ResultText.Text = result;
+                        return false;
+                    }
+                    else
+                        CurrentOperationText.Text = result;
+                    return true;
                 }
-                   // ButtonResult_Click(sender, e);
-                else
-                    CurrentOperationText.Text = result;
-                return true;
             }
             return true;
         }
 
         private void ButtonResult_Click(object sender, RoutedEventArgs e)
         {
-            var operation = CurrentOperationText.Text;
-
-            
+            var operation = CurrentOperationText.Text;    
             string result = CalculateResult(operation);
             ResultText.Text = result;
             memory = result;
@@ -98,85 +101,103 @@ namespace calculator
 
         private void ButtonDelete_Click(object sender, RoutedEventArgs e)
         {
-            string text = CurrentOperationText.Text.Remove(CurrentOperationText.Text.Length - 1);
-            CurrentOperationText.Text = text;
+            if (CurrentOperationText.Text != string.Empty)
+            {
+                string text = CurrentOperationText.Text.Remove(CurrentOperationText.Text.Length - 1);
+                CurrentOperationText.Text = text;
+            }
         }
 
-        private bool ContainsOperation(string operation) => operation.Contains("-") || operation.Contains("+") || operation.Contains("^") || operation.Contains("/") || operation.Contains("sqrt") || operation.Contains("*");
-               
-        private string CalculateResult(string operation)
-        {
-            if (operation.Contains("+"))
-            {
-                string [] elements = operation.Split('+');
 
-                 if (elements[0] != string.Empty && elements[1] != string.Empty)
-                 return (double.Parse(elements[0]) + double.Parse(elements[1])).ToString();
-                else
-                    return operation;
-            }
-            if (operation.Contains("-"))
-            {
-                string[] elements = operation.Split('-');
-                if(elements[0] != string.Empty && elements[1] != string.Empty)
-                    return (double.Parse(elements[0]) - double.Parse(elements[1])).ToString();
-                else
-                    return operation;    
-            }
-            if (operation.Contains("*"))
-            {
-                string[] elements = operation.Split('*');
-                if (elements[0] != string.Empty && elements[1] != string.Empty)
-                    return (double.Parse(elements[0]) * double.Parse(elements[1])).ToString();
-                else
-                    return operation;
-            }
-            if (operation.Contains("^"))
-            {
-                string[] elements = operation.Split('^');
-                if (elements[0] != string.Empty && elements[1] != string.Empty)
-                    return System.Math.Pow(double.Parse(elements[0]),double.Parse(elements[1])).ToString();
-                else
-                    return "error";
-            }
-            if (operation.Contains("/"))
-            {
-                string[] elements = operation.Split('/');
-                if (double.Parse(elements[1]) != 0)
-                    if (elements[0] != string.Empty && elements[1] != string.Empty)
-                    {
-                        return (double.Parse(elements[0]) / double.Parse(elements[1])).ToString();
-                    }
-                    else return operation;
-                else return "error";
-            }
-            if (operation.Contains("sqrt"))
-            {
-                string[] elements = operation.Split("sqrt");
-
-                if (elements[0] != string.Empty && elements[1] != string.Empty)
-                { 
-                    if (double.Parse(elements[1]) > 0)
-                        return System.Math.Sqrt(double.Parse(elements[1])).ToString();
-                }
-               else
-                    return "error";
-                
-            }
-
-            return "error";
-        }
-
-        private void ButtonMakeDouble_Click(object sender, RoutedEventArgs e)
-        {
-            CurrentOperationText.Text += ",";
-        }
+        private void ButtonMakeDouble_Click(object sender, RoutedEventArgs e) => CurrentOperationText.Text += ",";
+        
 
         private void ButtonMC_Click(object sender, RoutedEventArgs e)
         {
-            if (ContainsOperation((CurrentOperationText.Text[(CurrentOperationText.Text).Length - 1]).ToString()) || CurrentOperationText.Text == string.Empty)
-                CurrentOperationText.Text += memory;
+            if (memory != string.Empty)
+            {
+                if (CurrentOperationText.Text != string.Empty)
+                {
+                    if (ContainsOperation((CurrentOperationText.Text[(CurrentOperationText.Text).Length - 1]).ToString()) || CurrentOperationText.Text == string.Empty)
+                        CurrentOperationText.Text += memory;
+                }
+                else
+                    CurrentOperationText.Text += memory;
+            }
+        }
 
+        private bool ContainsOperation(string operation) => operation.Contains("-") || operation.Contains("+") || operation.Contains("^") || operation.Contains("/") || operation.Contains("*");
+               
+        private string CalculateResult(string operation)
+        {       
+            if (operation != string.Empty)
+            {
+                if (operation[0] != ',')
+                {
+                    if (ContainsOperation((operation[operation.Length - 1].ToString())) || operation[operation.Length - 1] == ',')
+                        operation = operation.Remove(operation.Length - 1);
+
+                    if (operation.Contains("+"))
+                    {
+                        string[] elements = operation.Split('+');
+
+                        if (elements[0] != string.Empty && elements[1] != string.Empty)
+                            return (double.Parse(elements[0]) + double.Parse(elements[1])).ToString();
+                        else
+                            return "error";
+                    }
+                    if (operation.Contains("-"))
+                    {
+                        string[] elements = operation.Split('-');
+                        if (elements[0] != string.Empty && elements[1] != string.Empty)
+                            return (double.Parse(elements[0]) - double.Parse(elements[1])).ToString();
+                        else
+                            return "error";
+                    }
+                    if (operation.Contains("*"))
+                    {
+                        string[] elements = operation.Split('*');
+                        if (elements[0] != string.Empty && elements[1] != string.Empty)
+                            return (double.Parse(elements[0]) * double.Parse(elements[1])).ToString();
+                        else
+                            return "error";
+                    }
+                    if (operation.Contains("^"))
+                    {
+                        string[] elements = operation.Split('^');
+                        if (elements[0] != string.Empty && elements[1] != string.Empty)
+                            return System.Math.Pow(double.Parse(elements[0]), double.Parse(elements[1])).ToString();
+                        else
+                            return "error";
+                    }
+                    if (operation.Contains("/"))
+                    {
+                        string[] elements = operation.Split('/');
+                        if (double.Parse(elements[1]) != 0)
+                            if (elements[0] != string.Empty && elements[1] != string.Empty)
+                            {
+                                return (double.Parse(elements[0]) / double.Parse(elements[1])).ToString();
+                            }
+                            else return operation;
+                        else return "error";
+                    }
+                    if (operation.Contains("sqrt"))
+                    {
+                        string[] elements = operation.Split("sqrt");
+
+                        if (elements[0] != string.Empty)
+                        {
+                            if (double.Parse(elements[0]) >= 0)
+                                return System.Math.Sqrt(double.Parse(elements[0])).ToString();
+                        }
+                        else
+                            return "error";
+                    }
+                }
+                else
+                    operation = operation.Remove(0);
+            }
+            return operation;
         }
     }
 }
